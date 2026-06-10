3c7f4b6849dad249d0d5763127fbb0df83b277b6
milisrivastav13 <milisrivastav13@gmail.com>
2026-06-10 19:45:09 +0530

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -1,16 +1,12 @@
-from sumy.parsers.plaintext import PlaintextParser
-from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.lsa import LsaSummarizer
-import nltk
-
-try:
-    nltk.data.find("tokenizers/punkt")
-except:
-    nltk.download("punkt")
 def summarize_notes(text):
-    parser = PlaintextParser.from_string(text, Tokenizer("english"))
-    summarizer = LsaSummarizer()
 
-    summary = summarizer(parser.document, 3)
+    sentences = text.split(".")
 
-    return " ".join([str(sentence) for sentence in summary])
+    sentences = [s.strip() for s in sentences if s.strip()]
+
+    if len(sentences) <= 3:
+        return ". ".join(sentences)
+
+    summary = ". ".join(sentences[:3])
+
+    return summary + "."
